24db6bd4ae9e738479659d1baef9ab1f6081b567
Tiago Pinto <up202206280@up.pt>
2023-12-18 17:11:47 +0000

    Some new parse functions and structure completed

--- Projeto - Air Travel Flight Management System/Classes/Data.cpp
+++ Projeto - Air Travel Flight Management System/Classes/Data.cpp
@@ -4,7 +4,7 @@
 
 #include "Data.h"
 using namespace std;
-
+/*
 vector<vector<string>> Data::readCSV(const std::string& filename) {
     std::vector<std::vector<std::string>> data;
 
@@ -30,15 +30,89 @@ vector<vector<string>> Data::readCSV(const std::string& filename) {
     file.close();
     return data;
 }
-
+*/
 std::vector<Airline> Data::readAirlines(const std::string& filename) {
-    // Implemente a leitura do arquivo CSV para Airlines
+    std::vector<Airline> airlines;
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo de Airlines." << std::endl;
+        return airlines;
+    }
+
+    std::string line;
+    std::getline(file, line);
+
+    while (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string code, name, callsign, country;
+        std::getline(ss, code, ',');
+        std::getline(ss, name, ',');
+        std::getline(ss, callsign, ',');
+        std::getline(ss, country, ',');
+
+        airlines.push_back(Airline{code, name, callsign, country});
+    }
+
+    file.close();
+    return airlines;
 }
 
 std::vector<Airport> Data::readAirports(const std::string& filename) {
-    // Implemente a leitura do arquivo CSV para Airports
+    std::vector<Airport> airports;
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo de Airports." << std::endl;
+        return airports;
+    }
+
+    std::string line;
+    std::getline(file, line);
+
+    while (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string code, name, city, country;
+        double latitude, longitude;
+
+        std::getline(ss, code, ',');
+        std::getline(ss, name, ',');
+        std::getline(ss, city, ',');
+        std::getline(ss, country, ',');
+        ss >> latitude; ss.ignore();
+        ss >> longitude; ss.ignore();
+
+        airports.push_back(Airport{code, name, city, country, latitude, longitude});
+    }
+
+    file.close();
+    return airports;
 }
 
 std::vector<Flight> Data::readFlights(const std::string& filename) {
-    // Implemente a leitura do arquivo CSV para Flights
+    std::vector<Flight> flights;
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+
+        std::cerr << "Erro ao abrir o arquivo de Flights." << std::endl;
+        return flights;
+    }
+
+    std::string line;
+    std::getline(file, line);
+
+    while (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string source, target, airline;
+
+        std::getline(ss, source, ',');
+        std::getline(ss, target, ',');
+        std::getline(ss, airline, ',');
+
+        flights.push_back(Flight{source, target, airline});
+    }
+
+    file.close();
+    return flights;
 }
--- Projeto - Air Travel Flight Management System/cmake-build-debug/cmake_install.cmake
+++ Projeto - Air Travel Flight Management System/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/tiago/clion/Projeto - Air Travel Flight Management System
+# Install script for directory: /home/tiago/Projeto---Air-Travel-Flight-Management-System/Projeto - Air Travel Flight Management System
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/tiago/clion/Projeto - Air Travel Flight Management System/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/tiago/Projeto---Air-Travel-Flight-Management-System/Projeto - Air Travel Flight Management System/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
